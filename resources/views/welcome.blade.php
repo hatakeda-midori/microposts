e53ce38d84cc0298fea4968ec84bcee864ea787c
m.hatakeda <hatakeda547867@outlook.jp>
2020-06-27 10:15:01 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            {{-- ユーザー登録ページへのリンク --}}
+            {!! link_to_route('sinup.get' , 'Sing up now!' , [], ['class' => 'btn btn-lg btn-primary7]) !!}
         </div>
     </div>
 @endsection
