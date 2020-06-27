419a792c885f8a78e5847e61c686eed64eb013ee
m.hatakeda <hatakeda547867@outlook.jp>
2020-06-27 13:13:48 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
             {{-- ユーザー登録ページへのリンク --}}
-            {!! link_to_route('sinup.get' , 'Sing up now!' , [], ['class' => 'btn btn-lg btn-primary7]) !!}
+            {!! link_to_route('signup.get' , 'Sign up now!' , [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
